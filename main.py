746cd8a041f1a6a5b2bad375abb26455ece9f9d0
GypsyGuro <baitoro2005@gmail.com>
2025-11-05 19:57:40 +0600

    add flask

---
+++ main.py
@@ -0,0 +1,25 @@
+"""chereshnya"""
+
+
+
+from flask import Flask
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return( 'Yo maboy')
+
+
+@app.route("/about_me")
+def about_me():
+    return "wassub bro"
+
+
+@app.route("/guro")
+def guro():
+    return "qwerty"
+
+if __name__ == '__main__':
+    app.run(debug=True)
